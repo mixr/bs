f537a35a1b2c9d0cd338bef8fff9ac5623c04fd6
Reiner Dieterich <reiner@mixr.de>
2010-03-11 13:03:14 +0100

    initial commit

---
+++ bs.rb
@@ -0,0 +1,22 @@
+# bs.rb
+require "rubygems"
+require "sinatra"
+require "faker"
+require "linguistics"
+
+get "/" do
+  init_linguistics
+  bs = Faker::Company.bs.split(" ")
+  bs = participling_first_word(bs)
+  "#{bs.join(" ")}!"
+end
+
+# turn the first word into present participle and capitalize it
+def participling_first_word(bs)
+  bs.insert( 0, bs.shift.capitalize.en.present_participle )
+end
+
+# initializes linguistics to use english
+def init_linguistics
+  Linguistics::use(:en)
+end
